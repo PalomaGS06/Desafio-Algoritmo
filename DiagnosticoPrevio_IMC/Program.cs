37a79d30edaea20c3843bf40d76ffb05074a9c51
Paloma Souza <paloma.souza112@gmail.com>
2022-06-17 00:07:35 -0300

    Adição de escolhas e condições na mensagem de retornar ou finalizar o programa

--- DiagnosticoPrevio_IMC/Program.cs
+++ DiagnosticoPrevio_IMC/Program.cs
@@ -13,132 +13,146 @@ namespace DiagnosticoPrevio_IMC
             int idade;
             string nome, sexo, categoria = null, classificacao = null, riscos = null, recomendacao = null, tentativa, enter;
 
-            do {
+            do
+            {
 
-                        //inserindo dados dentro de variáveis:
-                 Console.WriteLine(" \t Seja Bem Vindo(a)! Aperte a tecla ENTER para começar! \n ");
-                 enter = Console.ReadLine();
+                //inserindo dados dentro de variáveis:
+                Console.WriteLine(" \t Seja Bem Vindo(a)! Aperte a tecla ENTER para começar! \n ");
+                enter = Console.ReadLine();
 
-                 Console.Write( " Para gerarmos seu Diagnóstico Prévio, preencha seus dados abaixo: \n"); 
+                Console.Write(" Para gerarmos seu Diagnóstico Prévio, preencha seus dados abaixo: \n");
 
 
-                    //Nome
+                //Nome
                 Console.Write("\n Digite seu nome completo: ");
                 nome = Console.ReadLine();
 
-            while (string.IsNullOrWhiteSpace(nome))
+                while (string.IsNullOrWhiteSpace(nome))
                 {
                     Console.Write("Nome inexistente! Por favor, insira novamente: ");
                     nome = Console.ReadLine();
                 }
 
-                    //Sexo
+                //Sexo
                 Console.Write("\n Digite seu sexo (M para masculino e F para feminino): ");
                 sexo = Console.ReadLine();
 
-            while (sexo.ToUpper() != "F" && sexo.ToUpper() != "M")
+                while (sexo.ToUpper() != "F" && sexo.ToUpper() != "M")
 
-            {
-                Console.Clear();
+                {
+                    Console.Clear();
 
-                Console.Write("Dado inválido! por favor, tente novamente: ");
-                sexo = Console.ReadLine();
+                    Console.Write("Dado inválido! por favor, tente novamente: ");
+                    sexo = Console.ReadLine();
 
-            }
+                }
 
-            if (sexo.ToUpper() == "F")
-            {
-                sexo = "Feminino";
-            }
-            else if (sexo.ToUpper() == "M")
-            {
-                sexo = "Masculino";
-            }
+                if (sexo.ToUpper() == "F")
+                {
+                    sexo = "Feminino";
+                }
+                else if (sexo.ToUpper() == "M")
+                {
+                    sexo = "Masculino";
+                }
 
-                    //Idade
+                //Idade
                 Console.Write("\n Digite sua idade: ");
                 idade = int.Parse(Console.ReadLine());
 
-            while (idade <= 0)
-            {
-                Console.Clear();
+                while (idade <= 0 && idade > 150)
+                {
+                    Console.Clear();
 
-                Console.Write("Idade inválida! Por favor, tente novamente: ");
-                idade = int.Parse(Console.ReadLine());
-            }
-            
-           
-                    //Altura
+                    Console.Write("Idade inválida! Por favor, tente novamente: ");
+                    idade = int.Parse(Console.ReadLine());
+                }
+
+
+                //Altura
                 Console.Write("\n Digite sua altura, em metros(e utilize vírgula (,) para separar as medidas): ");
                 altura = double.Parse(Console.ReadLine().Replace(".", ",").ToString());
                 Thread.CurrentThread.CurrentCulture = new CultureInfo("pt");
 
-            while (altura <= 0 || altura >= 4.0)
-            {
-                Console.Clear();
+                while (altura <= 0 || altura >= 4.0)
+                {
+                    Console.Clear();
 
-                Console.Write("Altura inválida! Por favor, tente novamente: ");
-                altura = double.Parse(Console.ReadLine().Replace(".", ",").ToString());
-                Thread.CurrentThread.CurrentCulture = new CultureInfo("pt");
-            }
+                    Console.Write("Altura inválida! Por favor, tente novamente: ");
+                    altura = double.Parse(Console.ReadLine().Replace(".", ",").ToString());
+                    Thread.CurrentThread.CurrentCulture = new CultureInfo("pt");
+                }
 
-                    //Peso
+                //Peso
                 Console.Write("\n Digite seu peso, em Kg(e utilize vírgula (,) para separar o valor do peso): ");
                 peso = double.Parse(Console.ReadLine().Replace(".", ",").ToString());
                 Thread.CurrentThread.CurrentCulture = new CultureInfo("pt");
 
-           while (peso <= 0 || peso >= 300)
-            {
-                Console.Clear();
+                while (peso <= 0 || peso >= 300)
+                {
+                    Console.Clear();
 
-                Console.Write("Peso inválido! Por favor, tente novamente: ");
-                peso = double.Parse(Console.ReadLine().Replace(".", ",").ToString());
-                Thread.CurrentThread.CurrentCulture = new CultureInfo("pt");
-            }
-            
-                    //Categoria
-            if (idade < 12) { categoria = "Infantil"; }
-            if (idade >= 12 && idade <= 20) { categoria = "Juvenil"; }
-            if (idade >= 21 && idade <= 65) { categoria = "Adulto"; }
-            if (idade > 65) { categoria = "Idoso"; }
+                    Console.Write("Peso inválido! Por favor, tente novamente: ");
+                    peso = double.Parse(Console.ReadLine().Replace(".", ",").ToString());
+                    Thread.CurrentThread.CurrentCulture = new CultureInfo("pt");
+                }
+
+                //Categoria
+                if (idade < 12) { categoria = "Infantil"; }
+                if (idade >= 12 && idade <= 20) { categoria = "Juvenil"; }
+                if (idade >= 21 && idade <= 65) { categoria = "Adulto"; }
+                if (idade > 65) { categoria = "Idoso"; }
 
 
                 //Funções sendo chamadas e atribuídas por variáveis:
 
-                    imc = IMC(peso, altura);
+                imc = IMC(peso, altura);
 
-                    classificacao = Classificacao(imc);
+                classificacao = Classificacao(imc);
 
-                    riscos = Riscos(imc);
+                riscos = Riscos(imc);
 
-                    recomendacao = Recomendacao(imc);
-               
-    
-       
-            Console.Clear();
-
-            Console.WriteLine("\n\t DIAGNÓSTICO PRÉVIO\n\n ");
-
-            Console.WriteLine($" Nome:  {nome}\n " +
-                              $"Sexo:  {sexo}\n " +
-                              $"Idade:  {idade}\n " +
-                              $"Altura:  {Math.Round(altura, 2)} m\n " +
-                              $"Peso:  {Math.Round(peso, 2)} Kg\n " +
-                              $"Categoria:  {categoria}\n\n\n " +
-                              $"IMC Desejável:  entre 20 a 24\n\n " +
-                              $"Resultado IMC:  {Math.Round(imc,1)}\n\n " +
-                              $"Classificação IMC:  {classificacao}\n\n " +
-                              $"Riscos:  {riscos}\n\n " +
-                              $"Recomendação Inicial:  {recomendacao}\n\n\n ");
+                recomendacao = Recomendacao(imc);
 
 
-            Console.WriteLine("Deseja realizar o Diagnóstico Prévio outra vez? Digite 'S' para confirmar ou 'N' para finalizar: ");
-            tentativa = Console.ReadLine();
-                
-            Console.Clear();
-          }
+
+                Console.Clear();
+
+                Console.WriteLine("\n\t DIAGNÓSTICO PRÉVIO\n\n ");
+
+                Console.WriteLine($" Nome:  {nome}\n " +
+                                  $"Sexo:  {sexo}\n " +
+                                  $"Idade:  {idade}\n " +
+                                  $"Altura:  {Math.Round(altura, 2)} m\n " +
+                                  $"Peso:  {Math.Round(peso, 2)} Kg\n " +
+                                  $"Categoria:  {categoria}\n\n\n " +
+                                  $"IMC Desejável:  entre 20 a 24\n\n " +
+                                  $"Resultado IMC:  {Math.Round(imc, 1)}\n\n " +
+                                  $"Classificação IMC:  {classificacao}\n\n " +
+                                  $"Riscos:  {riscos}\n\n " +
+                                  $"Recomendação Inicial:  {recomendacao}\n\n\n ");
+
+
+                Console.WriteLine("\t\t*-* Deseja realizar o Diagnóstico Prévio outra vez? Digite 'S' para confirmar ou 'N' para finalizar: *-*");
+                tentativa = Console.ReadLine();
+
+                Console.Clear();
+           
+
+              while (tentativa.ToUpper() != "S" && tentativa.ToUpper() != "N")
+                {
+                Console.Write(" \tEscolha inválida. Por favor, digite 'S' para confirmar ou 'N' para finalizar o programa: \n");
+                tentativa = Console.ReadLine();
+                 }
+            }
             while (tentativa.ToUpper() == "S");
-            
+            Console.Clear();
+
+            if (tentativa.ToUpper() == "N")
+            {
+                Console.Write(" \tAgradecemos a sua preferência! Take Care!\n ");
+            }
+
 
         }
 
